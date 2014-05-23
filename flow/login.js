4e9da5d9f5c9999cdaee028097407b90ee2ebd9e
Matt Edelman <medelman@paypal.com>
2014-05-22 17:23:01 -0700

    adding login-button locator and actions

--- flow/login.js
+++ flow/login.js
@@ -1,11 +1,19 @@
 module.exports = function (nemo) {
 	return {
 		"login": function (user) {
-			nemo.view.login.email().clear();
-			nemo.view.login.email().sendKeys(user.emailAddress);
-			nemo.view.login.password().sendKeys(user.password);
-			nemo.view.login.button().click();
-			return nemo.view.login.logoutWait(10000);
+      return nemo.view.login.emailVisible().then(function(isVisible) {
+        if (!isVisible) {
+          return nemo.view.login.showLoginButton().click();
+        }
+        return true;
+      }).then(function() {
+        nemo.view.login.email().clear();
+        nemo.view.login.email().sendKeys(user.emailAddress);
+        nemo.view.login.password().sendKeys(user.password);
+        nemo.view.login.button().click();
+        return nemo.view.login.logoutWait(10000);
+      });
+
 		},
 		"logout": function() {
 		  return nemo.view.login.logout().click();
