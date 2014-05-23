d77d59fab389f9e19c82d5ead6a6cfbab618720e
Matt Edelman <mattedelman@gmail.com>
2014-05-22 17:05:33 -0700

    Update login.js

--- flow/login.js
+++ flow/login.js
@@ -1,1 +1,14 @@
-https://github.com/grawk/nemo-shared-objects.git
+module.exports = function (nemo) {
+	return {
+		"login": function (user) {
+			nemo.view.login.email().clear();
+			nemo.view.login.email().sendKeys(user.emailAddress);
+			nemo.view.login.password().sendKeys(user.password);
+			nemo.view.login.button().click();
+			return nemo.view.login.logoutWait(10000);
+		},
+		"logout": function() {
+		  return nemo.view.login.logout().click();
+		}
+	};
+};
